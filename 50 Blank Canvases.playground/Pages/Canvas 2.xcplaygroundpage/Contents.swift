52f0524dd9c7fb0f981fe7323b9a947dce021b2c
Homann Fritz <fhomann@lcs-mba3118.lcs.on.ca>
2017-11-28 16:49:26 -0500

    of my "my best work dock"

--- 50 Blank Canvases.playground/Pages/Canvas 2.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 2.xcplaygroundpage/Contents.swift
@@ -1,49 +1,160 @@
-//: [Previous](@previous) / [Next](@next)
-//: # A Blank Canvas
+//: [Previous](@previous)
+//: # Assignment
+//: Return to the plans you made at the start of this course for an image that used elements of repetition.
 //:
-//: Use this page to experiment. Have fun!
-/*:
- ## Required code
- 
- The following statements are required to make the playground run.
- 
- Please do not remove.
- */
+//: Mr. Gordon will be emailing this to you.
+//:
+//: Reproduce that image in code, below.
+//:
+//: Remember to commit and push your work often.
+//:
+//: ![commit_menu](commit_menu.png "Commit")
+//:
+//: ## Required code
+//: The following two statements are required to make the playground run. Please do not remove.
 import Cocoa
 import PlaygroundSupport
 
+//: ## Add your code below
+
+//// no fill
+//canvas.drawShapesWithFill = false
+//
+////loop to set horizontal position
+//for x in stride(from: 50, through: 450, by: 100) {
+//
+//    //loop to set vertical postion
+//    for y in stride(from: 450, through: 50, by: -100) {
+//
+//        //draw 5 squares
+//        for size in stride(from: 100, through: 20, by: -20) {
+//
+//
+//            canvas.drawRectangle(centreX: x, centreY: y, width: size, height: size)
+//        }
+//
+//
+//    }
+//
+//}
+
+
 // Create canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 500, height: 500)
+//
+//
+////draw top line (left to right)
+//for x in stride(from: 50, to: 150, by: 100) {
+//    for y in stride(from: 400, to: 500, by: 150) {
+//
+////draw top line (right to right)
+//        for x in stride(from: 150, to: 250, by: 100){
+//            for y in stride(from: 500, to: 400, by: 100){
+//
+////draw mid line (right to left)
+//                for x in stride(from: -50, to: 50, by: 100) {
+//                    for y in stride(from: 350, to: <#T##Strideable#>, by: <#T##Comparable & SignedNumeric#>)
 
-/*:
- ## Add your code below
- 
- Be sure to write human-readable code.
- 
- Use whitespace and comments as appropriate.
- */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
+
+canvas.lineColor = Color.red
+canvas.defaultLineWidth = 10
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+
+
+//canvas.drawLine(fromX: 0, fromY: 500, toX: 50, toY: 450, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 450, toX: 100, toY: 500, lineWidth: 1)
+//canvas.drawLine(fromX: 200, fromY: 500, toX: 250, toY: 450, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 450, toX: 300, toY: 500, lineWidth: 1)
+//canvas.drawLine(fromX: 400, fromY: 500, toX: 450, toY: 450, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 450, toX: 500, toY: 500, lineWidth: 1)
+//// Horizontal change with x
+
+
+//for x in stride(from: 0, through: 400, by: 200) {
+//
+//
+//    // Downward sloping line
+//    canvas.drawLine(fromX: x, fromY: 500, toX: x + 50, toY: 450)
+//    // Upward sloping line
+//    canvas.drawLine(fromX: x + 50, fromY: 450, toX: x + 100, toY: 500)
+//
+//}
+
+for y in stride(from: 450, through: 700, by: 200) {
+
+    
+    canvas.drawLine(fromX: -50, fromY: y + 100, toX: 50, toY: y)
+    
+}
 
 
 
-/*:
- ## Use source control
- To keep your work organized, and receive feedback, source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source_control.png "Source Control")
- */
+
+canvas.drawLine(fromX: 0, fromY: 450, toX: 50, toY: 400, lineWidth: 1)
+canvas.drawLine(fromX: 50, fromY: 400, toX: 150, toY: 500, lineWidth: 1)
+canvas.drawLine(fromX: 150, fromY: 500, toX: 250, toY: 400, lineWidth: 1)
+canvas.drawLine(fromX: 250, fromY: 400, toX: 350, toY: 500, lineWidth: 1)
+canvas.drawLine(fromX: 350, fromY: 500, toX: 450, toY: 400, lineWidth: 1)
+canvas.drawLine(fromX: 450, fromY: 400, toX: 500, toY: 450, lineWidth: 1)
+
+
+canvas.drawLine(fromX: 50, fromY: 400, toX: 0, toY: 350, lineWidth: 1)
+canvas.drawLine(fromX: 50, fromY: 400, toX: 150, toY: 300, lineWidth: 1)
+canvas.drawLine(fromX: 150, fromY: 300, toX: 250, toY: 400, lineWidth: 1)
+canvas.drawLine(fromX: 250, fromY: 400, toX: 350, toY: 300, lineWidth: 1)
+canvas.drawLine(fromX: 350, fromY: 300, toX: 450, toY: 400, lineWidth: 1)
+canvas.drawLine(fromX: 450, fromY: 400, toX: 500, toY: 350, lineWidth: 1)
+
+for y in stride(from: 250, through: 550, by: 300) {
+    for x in stride(from: 0, through: 400, by: 200) {
+        
+        //upward sloping lines
+        canvas.drawLine(fromX: -50 + x, fromY: y, toX: x + 50, toY: y + 100)
+        // downwards sloping lines
+        canvas.drawLine(fromX: x + 50, fromY: y + 100, toX: x + 50 + 100, toY: y)
+        //downward sloping lines
+        canvas.drawLine(fromX: -50 + x, fromY: 250, toX: x + 50, toY: 150)
+        //upwards sloping line
+        canvas.drawLine(fromX: x + 50, fromY: 150, toX: x + 150, toY: 250)
+        
+    }
+    
+}
+
+
+//canvas.drawLine(fromX: 0, fromY: 300, toX: 50, toY: 350, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 350, toX: 150, toY: 250, lineWidth: 1)
+//canvas.drawLine(fromX: 150, fromY: 250, toX: 250, toY: 350, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 350, toX: 350, toY: 250, lineWidth: 1)
+//canvas.drawLine(fromX: 350, fromY: 250, toX: 450, toY: 350, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 350, toX: 500, toY: 300, lineWidth: 1)
+
+//
+
+
+//canvas.drawLine(fromX: 0, fromY: 150, toX: 50, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 100, toX: 150, toY: 200, lineWidth: 1)
+//canvas.drawLine(fromX: 150, fromY: 200, toX: 250, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 100, toX: 350, toY: 200, lineWidth: 1)
+//canvas.drawLine(fromX: 350, fromY: 200, toX: 450, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 100, toX: 500, toY: 150, lineWidth: 1)
+//
+//canvas.drawLine(fromX: 0, fromY: 50, toX: 50, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 100, toX: 150, toY: 0, lineWidth: 1)
+//canvas.drawLine(fromX: 150, fromY: 0, toX: 250, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 100, toX: 350, toY: 0, lineWidth: 1)
+//canvas.drawLine(fromX: 350, fromY: 0, toX: 450, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 100, toX: 500, toY: 50, lineWidth: 1)
+//
+//
+//canvas.drawLine(fromX: 0, fromY: 0, toX: 50, toY: 50, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 50, toX: 100, toY: 0, lineWidth: 1)
+//canvas.drawLine(fromX: 200, fromY: 0, toX: 250, toY: 50, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 50, toX: 300, toY: 0, lineWidth: 1)
+//canvas.drawLine(fromX: 400, fromY: 0, toX: 450, toY: 50, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 50, toX: 500, toY: 0, lineWidth: 1)
 /*:
  ## Template code
- The code below is necessary to see results in the Assistant Editor at right.
- 
- Please do not remove.
- 
- If you don't see output, remember to show the Assistant Editor, and switch to Live View:
- 
- ![timeline](timeline.png "Timeline")
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
-// Don't remove the code below
 PlaygroundPage.current.liveView = canvas.imageView
-
--- 50 Blank Canvases.playground/Pages/Canvas 3.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 3.xcplaygroundpage/Contents.swift
@@ -1,49 +1,85 @@
-//: [Previous](@previous) / [Next](@next)
-//: # A Blank Canvas
-//:
-//: Use this page to experiment. Have fun!
-/*:
- ## Required code
- 
- The following statements are required to make the playground run.
- 
- Please do not remove.
- */
+// These are some required statements to make this playground work.
 import Cocoa
 import PlaygroundSupport
 
-// Create canvas
-let canvas = Canvas(width: 400, height: 300)
+// Create a new canvas
+let canvas = Canvas(width: 500, height: 500)
 
-/*:
- ## Add your code below
- 
- Be sure to write human-readable code.
- 
- Use whitespace and comments as appropriate.
- */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
+// Draw the axes
+canvas.drawAxes()
 
+// draw beckground
+canvas.fillColor = Color.init(hue: 200, saturation: 50, brightness: 100, alpha: 100)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
 
+// Draw the rays
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 1, fromY: 500, toX: 500, toY: 495, lineWidth: 6)
+canvas.lineColor = Color.orange
+canvas.drawLine(fromX: 1, fromY: 500, toX: 500, toY: 430, lineWidth: 6)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 1, fromY: 500, toX: 500, toY: 370, lineWidth: 6)
+canvas.lineColor = Color.orange
+canvas.drawLine(fromX: 1, fromY: 500, toX: 500, toY: 320, lineWidth: 6)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 1, fromY: 500, toX: 500, toY: 270, lineWidth: 6)
+canvas.lineColor = Color.orange
+canvas.drawLine(fromX: 1, fromY: 500, toX: 500, toY: 220, lineWidth: 6)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 1, fromY: 500, toX: 500, toY: 170, lineWidth: 6)
+canvas.lineColor = Color.orange
+canvas.drawLine(fromX: 1, fromY: 500, toX: 500, toY: 125, lineWidth: 6)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 1, fromY: 500, toX: 500, toY: 75, lineWidth: 6)
+canvas.lineColor = Color.orange
+canvas.drawLine(fromX: 1, fromY: 500, toX: 500, toY: 15, lineWidth: 6)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 1, fromY: 500, toX: 445, toY: 0, lineWidth: 6)
+canvas.lineColor = Color.orange
+canvas.drawLine(fromX: 1, fromY: 500, toX: 395, toY: 0, lineWidth: 6)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 1, fromY: 500, toX: 345, toY: 0, lineWidth: 6)
+canvas.lineColor = Color.orange
+canvas.drawLine(fromX: 1, fromY: 500, toX: 285, toY: 0, lineWidth: 6)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 1, fromY: 500, toX: 225, toY: 0, lineWidth: 6)
+canvas.lineColor = Color.orange
+canvas.drawLine(fromX: 1, fromY: 500, toX: 165, toY: 0, lineWidth: 6)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 1, fromY: 500, toX: 105, toY: 0, lineWidth: 6)
+canvas.lineColor = Color.orange
+canvas.drawLine(fromX: 1, fromY: 500, toX: 45, toY: 0, lineWidth: 6)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 1, fromY: 500, toX: 1, toY: 0, lineWidth: 8)
 
-/*:
- ## Use source control
- To keep your work organized, and receive feedback, source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source_control.png "Source Control")
- */
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right.
- 
- Please do not remove.
- 
- If you don't see output, remember to show the Assistant Editor, and switch to Live View:
- 
- ![timeline](timeline.png "Timeline")
- */
-// Don't remove the code below
+// Draw the sun
+canvas.fillColor = Color.yellow
+canvas.drawShapesWithBorders = false
+canvas.drawEllipse(centreX: 10, centreY: 480, width: 120, height: 120, borderWidth: 0)
+canvas.borderColor = Color.yellow
+
+// draw clouds
+canvas.fillColor = Color.white
+canvas.drawShapesWithBorders = false
+canvas.drawEllipse(centreX: 470, centreY: 480, width: 110, height: 110, borderWidth: 0)
+canvas.drawEllipse(centreX: 495, centreY: 430, width: 115, height: 115, borderWidth: 0)
+canvas.drawEllipse(centreX: 420, centreY: 490, width: 115, height: 115, borderWidth: 0)
+canvas.drawShapesWithBorders = true
+
+// draw rainbow
+canvas.borderColor = Color.red
+canvas.drawShapesWithFill = false
+canvas.drawEllipse(centreX: 250, centreY: 20, width: 460, height: 400, borderWidth: 40)
+canvas.borderColor = Color.orange
+canvas.drawEllipse(centreX: 250, centreY: 10, width: 420, height: 360, borderWidth: 40)
+canvas.borderColor = Color.yellow
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 380, height: 320, borderWidth: 40)
+canvas.borderColor = Color.green
+canvas.drawEllipse(centreX: 250, centreY: -10, width: 340, height: 280, borderWidth: 40)
+canvas.borderColor = Color.blue
+canvas.drawEllipse(centreX: 250, centreY: -20, width: 300, height: 240, borderWidth: 40)
+canvas.borderColor = Color.purple
+canvas.drawEllipse(centreX: 250, centreY: -30, width: 260, height: 220, borderWidth: 27)
+
+// This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
-
